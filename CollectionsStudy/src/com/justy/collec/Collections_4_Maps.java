82432182d847010220a2ac2e3c980f3b8efac8ac
justinkerala <justinkerala@yahoo.com>
2016-11-07 10:59:14 +0530

    sorting and removing duplicates in a  list with Treeset.

--- CollectionsStudy/src/com/justy/collec/Collections_4_Maps.java
+++ CollectionsStudy/src/com/justy/collec/Collections_4_Maps.java
@@ -6,11 +6,15 @@
  * @author  : justin */
 package com.justy.collec;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author justin 
@@ -39,6 +43,30 @@ public class Collections_4_Maps {
 		}
 	
 		
-	
+		Set<Integer>treesetImpl=new TreeSet<Integer>();
+		treesetImpl.add(21);
+		treesetImpl.add(2);
+		treesetImpl.add(1);
+		treesetImpl.add(20);
+		treesetImpl.add(10);
+		treesetImpl.add(11);
+		treesetImpl.forEach(System.out::println);
+		
+		//Using set for filtering and sorting a list. 
+ 		
+		List<Integer>intList=new ArrayList<>();
+		Set<Integer>listFilter=new TreeSet<Integer>();
+		intList.add(233);
+		intList.add(232);
+		intList.add(232);
+		System.out.println("===intList");
+		intList.forEach(System.out::println);
+		listFilter.addAll(intList);
+		System.out.println("===SetList");
+		listFilter.forEach(System.out::println);
+		
+		
+		
+		
 	}
 }
